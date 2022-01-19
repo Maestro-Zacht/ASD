17725facada22f8c6833f3ea1c98aca29d99fbb4
Matteo Ghia <matteo.ghia@yahoo.it>
2022-01-19 16:00:56 +0100

    rimosso sorted

--- grafi_dag/tabella.c
+++ grafi_dag/tabella.c
@@ -5,7 +5,7 @@
 
 struct table_s {
 	char** tabella;		// implementazione tramite vettore perchè la dimensione del problema non richiede una struttura dati più flessibile come un BST
-	int maxN, N, sorted;
+	int maxN, N;
 };
 
 
@@ -13,7 +13,6 @@ st_table st_init(int N) {
 	st_table table = (struct table_s*)malloc(sizeof(struct table_s));
 	table->maxN = N;
 	table->N = 0;
-	table->sorted = 0;
 	table->tabella = (char**)malloc(N * sizeof(char*));
 	return table;
 }
@@ -22,7 +21,6 @@ st_table st_copy(st_table table) {
 	int i;
 	st_table new_table = st_init(table->maxN);
 	new_table->N = table->N;
-	new_table->sorted = table->sorted;
 	for (i = 0; i < table->N; i++)
 		new_table->tabella[i] = strdup(table->tabella[i]);
 
@@ -43,9 +41,6 @@ const char* st_search_by_index(st_table table, int index) {
 
 int st_search_by_id(st_table table, const char* id) {
 	int right = table->N - 1, left = 0, medium, cmp;
-
-	if (!table->sorted)
-		st_sort(table);
 
 	while (left <= right) {
 		medium = left + (right - left) / 2;
@@ -97,90 +92,9 @@ void st_sort(st_table table) {
 	char** aux = (char**)malloc(table->N * sizeof(char*));
 	merge_sort_r(table->tabella, aux, 0, table->N - 1);
 	free(aux);
-	table->sorted = 1;
 }
 
 void st_insert(st_table table, const char* id) {
-	if (table->N < table->maxN) {
+	if (table->N < table->maxN)
 		table->tabella[table->N++] = strdup(id);
-		table->sorted = 0;
-	}
 }
-
-
-
-
-
-//typedef struct bst_node* link;
-//
-//struct bst_node {
-//	char* id;
-//	link p, l, r;
-//	int N;
-//};
-//
-//struct table_s {
-//	link root, z;
-//};
-//
-//link new_node(const char* id, link p, link r, link l, int N) {
-//	link n = (link)malloc(sizeof(struct bst_node));
-//	n->l = l;
-//	n->r = r;
-//	n->p = p;
-//	n->N = N;
-//	n->id = (id != NULL) ? strdup(id) : NULL;
-//	return n;
-//}
-//
-//void node_free(link l) {
-//	if (l->id != NULL)
-//		free(l->id);
-//	free(l);
-//}
-//
-//st_table st_init() {
-//	st_table table = (struct table_s*)malloc(sizeof(struct table_s));
-//	table->z = new_node(NULL, NULL, NULL, NULL, 0);
-//	table->root = table->z;
-//	return table;
-//}
-//
-//void st_free_r(link l, link z) {
-//	if (l != z) {
-//		st_free_r(l->r, z);
-//		st_free_r(l->l, z);
-//		node_free(l);
-//	}
-//}
-//
-//void st_free(st_table table) {
-//	if (table != NULL) {
-//		st_free_r(table->root, table->z);
-//		node_free(table->z);
-//		free(table);
-//	}
-//}
-//
-//link select_r(link l, int k, link z) {
-//	int t;
-//	if (l == z)
-//		return z;
-//	t = l->l->N;
-//	if (k < t)
-//		return select_r(l->l, k, z);
-//	if (k > t)
-//		return select_r(l->r, k - t - 1, z);
-//	return l;
-//}
-//
-//const char* st_search_by_index(st_table table, int index) {
-//	link node = select_r(table->root, index, table->z);
-//	return node->id;
-//}
-//
-//
-//
-//int st_search_by_id(st_table table, const char* id) {
-//	//TODO
-//}
